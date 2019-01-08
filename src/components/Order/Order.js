24fbf0cf2303cc9c028eb4db342738f2b971642d
Scallywag <scallywagjohn@outlook.com>
2019-01-08 13:54:19 +0000

    Routes Finished

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,24 @@
 import   React from 'react';
 import classes from './Order.module.css'
  const order = (props) => {
+      const ingredients=[]
+      for(let obj in props.ingredients){
+          ingredients.push({name:obj,amount:props.ingredients[obj]})
+      }
+      const ingredientOutput=ingredients.map(item=>
+        <span key ={item.name}
+        style ={{
+            textTransform:'capitalize',
+            margin:'0 4px',
+            padding:'5px 20px',
+            display:'inline-block',
+            border:'1px solid #ccc'
+        }}> 
+        {item.name} ({item.amount})</span>)
     return (
         <div className ={classes.Order}>
-           <p>Ingredients: Salad[1]</p> 
-           <p>Price: <strong>£6.45</strong></p>
+           <p>Ingredients: {ingredientOutput}</p> 
+           <p>Price: <strong>£{props.price.toFixed(2)}</strong></p>
         </div>
     );
 };
